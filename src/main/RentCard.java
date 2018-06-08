9226a898999c18c3364977d093fc79994bc15c9f
Ryan Coulson <contact@ryan-coulson.com>
2018-06-07 23:02:38 -0400

    added card info for rent cards

--- src/main/RentCard.java
+++ src/main/RentCard.java
@@ -29,49 +29,67 @@ public class RentCard extends Card {
 		Font oldFont = g.getFont();
 		Stroke oldStroke = g.getStroke();
 		
-		
-		// Draw physical card
-		g.setColor(Color.WHITE);
-		g.fillRect(x, y, 100, 160);
-		
-		g.setColor(Color.BLACK);
-		g.setStroke(new BasicStroke(2));
-		g.drawRect(x, y, 100, 160);
+		if(this.hovering) {
+			// Draw physical card
+			g.setColor(new Color(15, 15, 15));
+			g.fillRect(x, y, 100, 160);
+			
+			g.setColor(Color.BLACK);
+			g.setStroke(new BasicStroke(2));
+			g.drawRect(x, y, 100, 160);
+			
+			g.setColor(Color.WHITE);
+			g.setFont(new Font("Dialog", Font.PLAIN, 12));
 
-		
-		// Draw card name
-		g.setColor(this.color_primary);
-		g.fillRect(x+20, y+20, 60, 27);
-		
-		g.setColor(this.color_secondary);
-		g.fillRect(x+20, y+45, 60, 27);
-		
-		g.setColor(Color.WHITE);
-		g.setStroke(new BasicStroke(12));
-		g.drawOval(x+16,y+11, 68, 69);
-		
-		g.fillOval(x+30, y+26, 40,40);
-		
-		g.setColor(Color.BLACK);		
-		g.setFont(new Font("Dialog", Font.BOLD, 12));
-		g.drawString("Rent", x+37, y+50);
-		
-		// Card text
-		g.setFont(new Font("Dialog", Font.PLAIN, 9));
-		g.drawString("Your opponent must", x+7, y+90);
-		g.drawString("pay you rent for", x+17, y+100);
-		g.drawString("properties you own", x+12, y+110);
-		g.drawString("in these colours.", x+18, y+120);
+			g.drawString("Take money from", x+3, y+40);
+			g.drawString("your opponent", x+12, y+55);
 
-		g.setFont(new Font("Dialog", Font.PLAIN, 13));
-		
-		// Draw price
-		g.drawString(Integer.toString(this.value) + "M", x+5, y+15);
-		
-		
-		// Draw card type
-		g.setFont(new Font("Dialog", Font.PLAIN, 11));
-		g.drawString("rent card", x + 30, y + 150);
+			g.drawString("Right-click to", x+13, y+140);
+			g.drawString("bank", x+37, y+155);
+		} else {
+			// Draw physical card
+			g.setColor(Color.WHITE);
+			g.fillRect(x, y, 100, 160);
+			
+			g.setColor(Color.BLACK);
+			g.setStroke(new BasicStroke(2));
+			g.drawRect(x, y, 100, 160);
+	
+			
+			// Draw card name
+			g.setColor(this.color_primary);
+			g.fillRect(x+20, y+20, 60, 27);
+			
+			g.setColor(this.color_secondary);
+			g.fillRect(x+20, y+45, 60, 27);
+			
+			g.setColor(Color.WHITE);
+			g.setStroke(new BasicStroke(12));
+			g.drawOval(x+16,y+11, 68, 69);
+			
+			g.fillOval(x+30, y+26, 40,40);
+			
+			g.setColor(Color.BLACK);		
+			g.setFont(new Font("Dialog", Font.BOLD, 12));
+			g.drawString("Rent", x+37, y+50);
+			
+			// Card text
+			g.setFont(new Font("Dialog", Font.PLAIN, 9));
+			g.drawString("Your opponent must", x+7, y+90);
+			g.drawString("pay you rent for", x+17, y+100);
+			g.drawString("properties you own", x+12, y+110);
+			g.drawString("in these colours.", x+18, y+120);
+	
+			g.setFont(new Font("Dialog", Font.PLAIN, 13));
+			
+			// Draw price
+			g.drawString(Integer.toString(this.value) + "M", x+5, y+15);
+			
+			
+			// Draw card type
+			g.setFont(new Font("Dialog", Font.PLAIN, 11));
+			g.drawString("rent card", x + 30, y + 150);
+		}
 		
 		g.setColor(oldColor);
 		g.setFont(oldFont);
